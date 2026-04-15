14d127a3cb2fa160f10459d142a5e2be24614607
astratow <davno_pora@mail.com>
2026-04-15 21:26:05 +0100

    add show

---
+++ resources/js/Pages/Posts/Show.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({ post: Object });
+</script>
+
+<template>
+    <div class="p-6">
+        <h1>{{ post.title }}</h1>
+        <p>{{ post.body }}</p>
+
+        <small>By {{ post.user.name }}</small>
+    </div>
+</template>
